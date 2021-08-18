5a49840d6f6d802a912996ae606cbc494cc10932
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-08-18 10:44:31 -0500

    - 3.6 phonebook

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -26,8 +27,57 @@ const persons = [
   },
 ];
 
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${
+      persons.length
+    }</p><p>${new Date()}</p>`
+  );
+});
+
 app.get('/api/persons', (req, res) => {
   res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing!',
+    });
+  }
+  if (persons.filter((p) => p.name === body.name).length > 0) {
+    return res.status(400).json({
+      error: 'name must be unique!',
+    });
+  }
+  const id = Math.floor(Math.random() * 100000000);
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id,
+  };
+
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
