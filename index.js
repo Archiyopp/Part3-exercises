70af0e2f2cce455c62e0c86cc1ae0d833b842651
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-08-18 11:26:11 -0500

    3.7 - 3.8 phonebook

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const { response } = require('express');
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 app.use(express.json());
+morgan.token('body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+});
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :body'
+  )
+);
 
 let persons = [
   {
