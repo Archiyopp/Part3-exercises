ed8b57f8868d2a7e16d40a9917a57af6f42ef79e
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-08-18 11:53:54 -0500

    procfile and cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 morgan.token('body', (req) => {
   if (req.method === 'POST') {
@@ -90,7 +92,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
