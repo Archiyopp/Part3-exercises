ce57545fb94fed4493c2cc367d385c36c57712d0
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-08-18 18:08:07 -0500

    - 3.18 phonebook

--- index.js
+++ index.js
@@ -43,11 +43,13 @@ let persons = [
 ];
 
 app.get('/info', (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${
-      persons.length
-    }</p><p>${new Date()}</p>`
-  );
+  Person.find({}).then((people) => {
+    res.send(
+      `<p>Phonebook has info for ${
+        people.length
+      }</p><p>${new Date()}</p>`
+    );
+  });
 });
 
 app.get('/api/persons', (req, res) => {
@@ -56,21 +58,19 @@ app.get('/api/persons', (req, res) => {
   });
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
 });
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body;
   if (!body.name || !body.number) {
     return res.status(400).json({
@@ -88,11 +88,55 @@ app.post('/api/persons', (req, res) => {
   });
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing!',
+    });
+  }
+
+  const updatedPerson = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, updatedPerson, {
+    new: true,
+  })
+    .then((updatedP) => {
+      res.json(updatedP);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
